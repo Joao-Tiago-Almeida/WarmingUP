2e682700cdb42499afa432475f97b21a7749b23a
Miguel Fazenda <miguelsfazenda@gmail.com>
2018-05-08 19:48:44 +0100

    Melhorei os menus e fiz com que ele lêsse os parametros do terminal

--- main.c
+++ main.c
@@ -1,38 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
 #include "estruturas.h"
 #include "lista.h"
 #include "menus.h"
-#include <string.h>
 
-
-
-int main ( void )
+void display_usage()
 {
-  int ano_a_analisar = -1, periodo_a_analisar = -1;
-  list_node_t* headListaDados = NULL;
+    printf("Argumentos invalidos!\n\tExemplo: ./WarmingUp –g -f1 tempcountries.csv -f2 tempcities.csv");
+}
 
-  headListaDados = create_list();
+void ler_argumentos(int argc, char *argv[], bool *modoGrafico, char **nomeFilePaises, char **nomeFileCidades) {
+    //Quando é lido -f1 ou -f2 esta variavel indica que
+    // o argumento seguinte é o nome do ficheiro
+    bool expectingFileName = false;
 
-  //Teste de como criar elementos na lista
-  for(int i = 0; i<2; i++) {
-      dados_temp a;
-      a.dt.ano = 1;
-      a.dt.mes = 1;
-      a.dt.dia = 1;
-      a.temp = 10.0f;
-      a.incerteza = 1.0f;
-      strcpy(a.pais, "Portugal");
-      strcpy(a.cidade, "Santarem");
-      a.latitude.angular = 1.0f;
-      a.latitude.direcao = NORTE;
-      insert_node(headListaDados, &a);
-  }
-  print_list(headListaDados);
+    for (int i = 1; i < argc; i++)
+    {
+        if(expectingFileName) {
+            if (strcmp(argv[i-1], "-f1") == 0) {
+                *nomeFilePaises = argv[i];
+            } else {
+                *nomeFilePaises = argv[i];
+            }
+            expectingFileName = false;
+        } else {
+            if (strcmp(argv[i], "-g") == 0)
+            {
+                *modoGrafico = true;
+            }
+            else if (strcmp(argv[i], "-f1") == 0 || strcmp(argv[i], "-f2") == 0)
+            {
+                expectingFileName = true;
+            }
+            else
+            {
+                display_usage();
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
 
-  menu_principal(&periodo_a_analisar, &ano_a_analisar);
+    //Caso o acabe em -fx sem nome do ficheiro ou não seja colocado o nome de um dos ficheiros
+    if(expectingFileName || *nomeFilePaises == NULL || *nomeFilePaises == NULL)
+    {
+        display_usage();
+        exit(EXIT_FAILURE);
+    }
+}
 
-  printf("END\n");
+void modoTextual(char *nomeFilePaises, char *nomeFileCidades) {
+    int ano_a_analisar = -1, periodo_a_analisar = -1;
+    list_node_t *headListaDados = NULL;
 
-  return EXIT_SUCCESS;
+    headListaDados = create_list();
+
+    //Teste de como criar elementos na lista
+    for (int i = 0; i < 2; i++)
+    {
+        dados_temp a;
+        a.dt.ano = 1;
+        a.dt.mes = 1;
+        a.dt.dia = 1;
+        a.temp = 10.0f;
+        a.incerteza = 1.0f;
+        strcpy(a.pais, "Portugal");
+        strcpy(a.cidade, "Santarem");
+        a.latitude.angular = 1.0f;
+        a.latitude.direcao = NORTE;
+        
+        insert_node(headListaDados, &a);
+    }
+    print_list(headListaDados);
+
+    menu_principal(&periodo_a_analisar, &ano_a_analisar);
+
+    printf("END\n");
+}
+
+int main(int argc, char *argv[])
+{
+    bool modoGrafico = false;
+    char *nomeFilePaises = NULL, *nomeFileCidades = NULL;
+    ler_argumentos(argc, argv, &modoGrafico, &nomeFilePaises, &nomeFileCidades);
+
+    if(modoGrafico) {
+        //Inicializar modo grafico
+        printf("Modo grafico!\n");
+    } else {
+        modoTextual(nomeFilePaises, nomeFileCidades);
+    }
+
+    return EXIT_SUCCESS;
 }
--- menus.c
+++ menus.c
@@ -1,178 +1,252 @@
 #include "menus.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-#define buffer 2
+#define BUFFER_SIZE 100
+
+#define MIN_ANO 0 //TODO ver se isto está correto
+#define MAX_ANO 2018
+
+//Le o que o utilizador escreveu e devolve o numero da alinea. Caso seja invalido devolve -1
+int perguntar_menu_choice()
+{
+    int alinea = 0;
+    char buf[BUFFER_SIZE];
+
+    fgets(buf, BUFFER_SIZE, stdin);
+    if (sscanf(buf, "%d", &alinea) == 0)
+    {
+        //TODO veriricar se o utilizador escrever algo do genero de "2 5" (isto deveria dar invalido)
+        alinea = -1;
+    }
+
+    return alinea;
+}
+
+int perguntar_anos_a_analisar()
+{
+    char buf[BUFFER_SIZE];
+    int numAnos = -1;
+
+    printf("Qual o periodo que pretende analisar [1-500]: ");
+
+    do
+    {
+        fgets(buf, BUFFER_SIZE, stdin);
+        if (sscanf(buf, "%d", &numAnos) == 0)
+        {
+            numAnos = -1;
+        }
+    } while (numAnos < 1 || 500 < numAnos);
+
+    return numAnos;
+}
+
+int perguntar_ano_a_analisar()
+{
+    char buf[BUFFER_SIZE];
+    int ano = -1;
+
+    printf("Qual o ano que pretende analisar [0-2018]:");
+
+    do
+    {
+        fgets(buf, BUFFER_SIZE, stdin);
+        if (sscanf(buf, "%d", &ano) == 0)
+        {
+            ano = -1;
+        }
+    } while (ano < MIN_ANO || MAX_ANO < ano);
+
+    return ano;
+}
+
+
 
 void menu_principal(int *_periodo_a_analisar, int *_ano_a_analisar)
 {
-  char buf[buffer];
-  int alinea = -1;
+    bool dentroDoMenu = true;
+    int alinea = -1;
 
-  printf("\n\tMenu Principal\n\nEscolha uma das opções seguintes:\n");
-  printf("1. Filtragem de dados;\n2. Histórico de Temperaturas;\n");
-  printf("3. Análise da temperatura por ano;\n4. Análise da temperatura global.\n");
-  printf("\n0. Sair do programa.\n");
-  do
-  {
-  fgets(buf, buffer, stdin);
-  sscanf(buf, "%d", &alinea);
-  }while(alinea < 0 || 5 < alinea );
-  switch (alinea) {
-    case 1: menu_filtragem_de_dados(_periodo_a_analisar, _ano_a_analisar);
-      break;
-    case 2: menu_historico_de_temperaturas(_periodo_a_analisar, _ano_a_analisar);
-      break;
-    case 3: menu_analise_da_temperatura_por_ano(_periodo_a_analisar, _ano_a_analisar);
-      break;
-    case 4: menu_analise_da_temperatura_global();
-      break;
-    case 0:
-      printf("Quit\n");
-      exit(EXIT_SUCCESS);
-      break;
-    default:
-      printf("\nOpção inválida\n");
-      return;
-      break;
+    while (dentroDoMenu)
+    {
+        printf("\n\tMenu Principal\n\nEscolha uma das opções seguintes:\n");
+        printf("1. Filtragem de dados;\n2. Histórico de Temperaturas;\n");
+        printf("3. Análise da temperatura por ano;\n4. Análise da temperatura global.\n");
+        printf("\n0. Sair do programa.\n");
+
+        alinea = perguntar_menu_choice();
+
+        switch (alinea)
+        {
+        case 1:
+            menu_filtragem_de_dados(_periodo_a_analisar, _ano_a_analisar);
+            break;
+        case 2:
+            menu_historico_de_temperaturas(_periodo_a_analisar, _ano_a_analisar);
+            break;
+        case 3:
+            menu_analise_da_temperatura_por_ano(_periodo_a_analisar, _ano_a_analisar);
+            break;
+        case 4:
+            menu_analise_da_temperatura_global();
+            break;
+        case 0:
+            printf("Quit\n");
+            exit(EXIT_SUCCESS);
+            break;
+        default:
+            printf("\nOpção inválida\n");
+            break;
+        }
     }
 }
 void menu_filtragem_de_dados(int *_periodo_a_analisar, int *_ano_a_analisar)
 {
-  char buf[buffer];
-  int alinea = -1;
+    bool dentroDoMenu = true;
+    int alinea = -1;
 
-  printf("\n\tMenu Filtragem de Dados\n\nEscolha uma das opções seguintes:\n");
-  printf("1. Limpa critéritos;\n2. Escolhe intervalos para analise;\n");
-  printf("3. Escolhe um mês.\n\n0. Volta ao Menu Principal.\n");
-  do {
-    fgets(buf, buffer, stdin);
-    sscanf(buf, "%d", &alinea);
-  } while(alinea < 0 || 4 < alinea);
-  switch (alinea) {
-    case 1: limpa_criteritos();
-      break;
-    case 2: escolhe_intervalos_para_analise();
-      break;
-    case 3: escolhe_mes();
-      break;
-    case 0: menu_principal(_periodo_a_analisar, _ano_a_analisar);
-      break;
-    default:
-      printf("\nOpção inválida\n");
-      return;
-      break;
+    while (dentroDoMenu)
+    {
+        printf("\n\tMenu Filtragem de Dados\n\nEscolha uma das opções seguintes:\n");
+        printf("1. Limpa critéritos;\n2. Escolhe intervalos para analise;\n");
+        printf("3. Escolhe um mês.\n\n0. Volta ao Menu Principal.\n");
+
+        alinea = perguntar_menu_choice();
+
+        switch (alinea)
+        {
+        case 1:
+            limpa_criteritos();
+            break;
+        case 2:
+            escolhe_intervalos_para_analise();
+            break;
+        case 3:
+            escolhe_mes();
+            break;
+        case 0:
+            dentroDoMenu = false; //Sai do menu, voltando para o anterior
+            break;
+        default:
+            printf("\nOpção inválida\n");
+            break;
+        }
     }
 }
-void menu_historico_de_temperaturas(int *_periodo_a_analisar , int *_ano_a_analisar)
+void menu_historico_de_temperaturas(int *_periodo_a_analisar, int *_ano_a_analisar)
 {
-  char buf[buffer], ano[buffer];
-  int alinea = -1;
-                                  //não sei o intervalo do periodo
-  printf("\n\tMenu Histórico de Temperaturas\n\nQual o periodo que pretende analisar [1-500]:\n");
-  *_periodo_a_analisar = -1;      //pode vir com outros valores de fora
-  do {
-    fgets(ano, buffer, stdin);
-    sscanf(ano, "%d", _periodo_a_analisar);
-  } while(*_periodo_a_analisar < 1 || 500 < *_periodo_a_analisar);
-  printf("período a analisar: %d\n", *_periodo_a_analisar);
+    bool dentroDoMenu = false;
+    int alinea = -1;
+    int periodo = -1;
 
-  printf("\n\nEscolha uma das opções seguintes:\n");
-  printf("1. Global: aplica-se a todos os países;\n2. Por País: aplica-se a um único país;\n");
-  printf("3. Por Cidade: aplica-se a uma única cidade.\n\n0. Volta ao Menu Principal.\n");
-  do{
-  fgets(buf, buffer, stdin);
-  sscanf(buf, "%d", &alinea);
-  }while ((alinea < 0 || 3 < alinea));
+    printf("\n\tMenu Histórico de Temperaturas\n\n");
 
-  switch (alinea) {
-    case 1: historico_de_temperaturas_global();
-      break;
-    case 2: historico_de_temperaturas_por_pais();
-      break;
-    case 3: historico_de_temperaturas_por_cidade();
-      break;
-    case 0:
-      menu_principal(_periodo_a_analisar, _ano_a_analisar);
-      *_periodo_a_analisar = -1;
-      break;
-    default:
-      printf("\nOpção inválida\n");
-      return;
-      break;
-    }
+    periodo = perguntar_anos_a_analisar();
+    printf("período(num de anos) a analisar: %d\n", *_periodo_a_analisar);
 
+    do
+    {
+        //Quando se escolhe qualquer opção ele sai logo do menu. No entanto se for opção
+        // invalida ele deve fazer loop
+        dentroDoMenu = false;
+
+        printf("\n\nEscolha uma das opções seguintes:\n");
+        printf("1. Global: aplica-se a todos os países;\n2. Por País: aplica-se a um único país;\n");
+        printf("3. Por Cidade: aplica-se a uma única cidade.\n\n0. Volta ao Menu Principal.\n");
+
+        alinea = perguntar_menu_choice();
+
+        switch (alinea)
+        {
+        case 1:
+            historico_de_temperaturas_global(periodo);
+            break;
+        case 2:
+            historico_de_temperaturas_por_pais(periodo);
+            break;
+        case 3:
+            historico_de_temperaturas_por_cidade(periodo);
+            break;
+        case 0:
+            *_periodo_a_analisar = -1;
+            break;
+        default:
+            printf("\nOpção inválida\n");
+            dentroDoMenu = true;
+            break;
+        }
+    } while (dentroDoMenu);
 }
-void menu_analise_da_temperatura_por_ano(int *_periodo_a_analisar , int *_ano_a_analisar)
+void menu_analise_da_temperatura_por_ano(int *_periodo_a_analisar, int *_ano_a_analisar)
 {
-  char buf[buffer], ano[buffer];
-  int alinea = -1;
+    bool dentroDoMenu = true;
+    int alinea = -1;
+    int ano;
 
-  //nao sei o intervalo dos anos
-  printf("\n\tMenu Análise da Temperatura por Ano\n\nQual o ano que pretende analisar [0-2018]:\n");
-  *_ano_a_analisar = -1;
+    while (dentroDoMenu)
+    {
+        printf("\n\tMenu Análise da Temperatura por Ano\n\n");
+        ano = perguntar_ano_a_analisar();
 
-  do {
-    fgets(ano, buffer, stdin);
-    sscanf(ano, "%d", _ano_a_analisar);
-  } while(*_ano_a_analisar < 0 || 2018 < *_ano_a_analisar);
+        printf("\n\nEscolha uma das opções seguintes:\n");
+        printf("1. Análise por País;\n2. Análise por cidade.\n");
+        printf("\n0. Volta ao Menu Principal.\n");
 
-  printf("\n\nEscolha uma das opções seguintes:\n");
-  printf("1. Análise por País;\n2. Análise por cidade.\n");
-  printf("\n0. Volta ao Menu Principal.\n");
-  do {
-    fgets(buf, buffer, stdin);
-    sscanf(buf, "%d", &alinea);
-  } while(alinea < 0 || 2 < alinea );
+        alinea = perguntar_menu_choice();
 
-  switch (alinea) {
-    case 1: analise_por_pais();
-      break;
-    case 2: analise_por_cidade();
-      break;
-    case 0:
-      menu_principal(_periodo_a_analisar, _ano_a_analisar);
-      *_ano_a_analisar = -1;;
-      break;
-    default:
-      printf("\nOpção inválida\n");
-      return;
-      break;
-  }
+        switch (alinea)
+        {
+        case 1:
+            analise_por_pais(ano);
+            break;
+        case 2:
+            analise_por_cidade(ano);
+            break;
+        case 0:
+            dentroDoMenu = false;
+            break;
+        default:
+            printf("\nOpção inválida\n");
+            break;
+        }
+    }
 }
 void menu_analise_da_temperatura_global()
 {
-  printf("\n\n\t---Análise da temperuta Global---\n\n");
+    printf("\n\n\t---Análise da temperuta Global---\n\n");
 }
 void limpa_criteritos()
 {
-  printf("\n\n\t---Limpa Critéritos---\n\n");
+    printf("\n\n\t---Limpa Critéritos---\n\n");
 }
 void escolhe_intervalos_para_analise()
 {
-  printf("\n\n\t---Escolhe intervalos para análise---\n\n");
+    printf("\n\n\t---Escolhe intervalos para análise---\n\n");
 }
 void escolhe_mes()
 {
-  printf("\n\n\t---Escolhe mês---\n\n");
+    printf("\n\n\t---Escolhe mês---\n\n");
 }
-void historico_de_temperaturas_global()
+void historico_de_temperaturas_global(int periodo)
 {
-  printf("\n\n\t---Histórico de Temperaturas Global---\n\n");
+    printf("\n\n\t---Histórico de Temperaturas Global---\n\n");
 }
-void historico_de_temperaturas_por_pais()
+void historico_de_temperaturas_por_pais(int periodo)
 {
-  printf("\n\n\t---Histórico de Temperaturas por País---\n\n");
+    printf("\n\n\t---Histórico de Temperaturas por País---\n\n");
 }
-void historico_de_temperaturas_por_cidade()
+void historico_de_temperaturas_por_cidade(int periodo)
 {
-  printf("\n\n\t---Histórico de Temperaturas por Cidade---\n\n");
+    printf("\n\n\t---Histórico de Temperaturas por Cidade---\n\n");
 }
-void analise_por_pais()
+void analise_por_pais(int ano)
 {
-  printf("\n\n\t---Análise por País---\n\n");
+    printf("\n\n\t---Análise por País---\n\n");
 }
-void analise_por_cidade()
+void analise_por_cidade(int ano)
 {
-  printf("\n\n\t---Análise por cidade---\n\n");
+    printf("\n\n\t---Análise por cidade---\n\n");
 }
